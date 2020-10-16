e8153185aff77e3e40f13931f8ecd22685f841ae
eguzman5 <eguzman5@visteon.com>
2020-10-16 12:39:31 -0500

    Added cositas

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
-int addTwoNumbers(int a, int b) {
-    return a + b;
+int addTwoNumbers(int a, int b, int& c) {
+    c = a + b;
 }
 
 int main() {
-    std::cout << addTwoNumbers(5, 5);
+    int a = 5;
+    int b = 5;
+    int c = addTwoNumbers(a,b);
+    std::cout << c;
     return 0;
 }
