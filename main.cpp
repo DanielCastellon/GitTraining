3a939d3983295dcc2c7e9ffa0d685caa432f7d8f
hcastell <hcastell@visteon.com>
2020-10-16 12:35:29 -0500

    DaCa: Add print function

--- main.cpp
+++ main.cpp
@@ -4,7 +4,14 @@ int addTwoNumbers(int a, int b) {
     return a + b;
 }
 
+void DaCaPrintName()
+{
+	printf("DaCa print");
+}
+
+
 int main() {
     std::cout << addTwoNumbers(5, 5);
+    std::cout << DacaPrintName();
     return 0;
 }
